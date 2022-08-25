332b8ab73627ac40419029e813c91a15a94f5225
Ing Orlando Barragan <orlando.barragan88@outlook.com>
2022-08-25 18:32:52 -0500

    Creacion Clases con Intellij Maven

---
+++ src/main/java/Entities/MovimientoDinero.java
@@ -0,0 +1,37 @@
+package Entities;
+
+public class MovimientoDinero {
+    private double monto;
+    private String concepto;
+    private String usuario;
+
+    public MovimientoDinero(double monto, String concepto, String usuario) {
+        this.monto = monto;
+        this.concepto = concepto;
+        this.usuario = usuario;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public String getConcepto() {
+        return concepto;
+    }
+
+    public void setConcepto(String concepto) {
+        this.concepto = concepto;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+}
---
+++ src/main/java/Repository/main.java
@@ -0,0 +1,25 @@
+package Repository;
+
+import Entities.*;
+
+public class main {
+    public static void main(String args[]){
+        Empresa em1 = new Empresa("Mintic","Bogota","12345", 123854);
+        System.out.println(em1.getNombre());
+        System.out.println(em1.getDireccion());
+        System.out.println(em1.getTelefono());
+        System.out.println(em1.getNit());
+
+        Empleado ep1 = new Empleado("Orlando Barragan", "orlando.barragan88@outlook.com", "Ministerio", "Administrador");
+        System.out.println(ep1.getNombre());
+        System.out.println(ep1.getCorreo());
+        System.out.println(ep1.getEmpresa());
+        System.out.println(ep1.getRol());
+
+        MovimientoDinero mv1 = new MovimientoDinero(1500000, "Pago administracion", "OrlandoBarragan88");
+        System.out.println(mv1.getMonto());
+        System.out.println(mv1.getConcepto());
+        System.out.println(mv1.getUsuario());
+
+    }
+}
